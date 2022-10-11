01661c80ba7bebea864af7eca9bd7ede24d91122
Mixss <michalziemiec@wp.pl>
2022-10-11 11:49:50 +0200

    Initial commit

---
+++ src/main/java/esovisco/enauczaniefeed/EnauczanieFeedApplication.java
@@ -0,0 +1,13 @@
+package esovisco.enauczaniefeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnauczanieFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnauczanieFeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/esovisco/enauczaniefeed/EnauczanieFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package esovisco.enauczaniefeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnauczanieFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
